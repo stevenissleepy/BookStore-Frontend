b115a9cfe14749bd7d2cdeba17d4d0e6547f436f
steven <1085320540@qq.com>
2025-04-03 10:17:59 +0800

    feat: 完成了 layout

--- src/components/layout.jsx
+++ src/components/layout.jsx
@@ -6,33 +6,16 @@ import MyFooter from "./footer";
 const { Content } = Layout
 
 
-function MyLayout() {
-  const {
-    token: { colorBgContainer, borderRadiusLG },
-  } = theme.useToken()
+function MyLayout(children) {
 
   return (
-    <Layout>
+    <Layout className="my-layout">
       {/* Header */}
       <MyHeader />
       
       {/* Content */}
-      <Content style={{ padding: "0 48px" }}>
-        <Breadcrumb style={{ margin: "16px 0" }}>
-          <Breadcrumb.Item>Home</Breadcrumb.Item>
-          <Breadcrumb.Item>List</Breadcrumb.Item>
-          <Breadcrumb.Item>App</Breadcrumb.Item>
-        </Breadcrumb>
-        <div
-          style={{
-            background: colorBgContainer,
-            minHeight: 280,
-            padding: 24,
-            borderRadius: borderRadiusLG,
-          }}
-        >
-          Content
-        </div>
+      <Content className="content-container">
+        {children}
       </Content>
 
       {/* Footer */}
