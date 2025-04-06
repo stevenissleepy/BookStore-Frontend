5dae036f9107d4e5c7a5a58a92fcd48df06004cc
steven <1085320540@qq.com>
2025-04-06 18:37:47 +0800

    feat: add label for select

--- src/components/book_list.jsx
+++ src/components/book_list.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react"
 import { List, Space, Row, Col, Select } from "antd"
+import { MenuOutlined } from "@ant-design/icons"
 
 import BookCard from "./book_card"
 
@@ -25,18 +26,21 @@ function BookList({ books }) {
     if (selectedCategories.length === 0) {
       setFilteredBooks(books)
     } else {
-      const filtered = books.filter((book) =>
-        selectedCategories.includes(book.category)
-      )
+      const filtered = books.filter((book) => selectedCategories.includes(book.category))
       setFilteredBooks(filtered)
     }
   }
 
   return (
     <Space direction="vertical" style={{ width: "100%" }}>
-      <Row justify="space-between" align="middle" style={{ marginBottom: "20px", marginTop: "30px" }}>
-        <Col>
+      <Row justify="start" align="middle" style={{ marginBottom: "20px", marginTop: "30px" }}>
+        <Col span={2}>
           <div className="book-list-header-left">{filteredBooks.length} Books</div>
+        </Col>
+        <Col span={2} offset={16} style={{ textAlign: "right" }}>
+          <h3 className="book-list-header-right">
+            <div style={{marginRight: "10px"}}><MenuOutlined /></div> Filter
+          </h3>
         </Col>
         <Col span={4}>
           <Select
