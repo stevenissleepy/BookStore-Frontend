dd61480b603ab53bf2e087d53b5ddd0f06321ea3
steven <1085320540@qq.com>
2025-04-06 17:37:57 +0800

    feat: 使用真实书籍信息

--- src/components/book_card.jsx
+++ src/components/book_card.jsx
@@ -4,7 +4,14 @@ import { Link } from "react-router-dom"
 function BookCard({ book }) {
   return (
     <Link to={`/book/${book.id}`}>
-      <Card hoverable cover={<img alt="example" src={book.cover} />}>
+      <Card
+        hoverable
+        cover={
+          <div className="book-card-img-wrapper">
+            <img className="book-card-img" alt="example" src={book.cover} />
+          </div>
+        }
+      >
         <Card.Meta title={book.title} description={`${book.price} 元`} />
       </Card>
     </Link>
--- src/components/book_list.jsx
+++ src/components/book_list.jsx
@@ -17,8 +17,6 @@ function BookList({ books }) {
           </List.Item>
         )}
       />
-      {/* pagination */}
-      
     </Space>
   )
 }
