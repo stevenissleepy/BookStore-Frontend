e2666b6e91f7fc731286eee197fdad8add70beed
steven <1085320540@qq.com>
2025-04-07 17:21:46 +0800

    feat(profile_address): 通过提供的 getAddress 接口获取 addresses

--- src/components/profile_address.jsx
+++ src/components/profile_address.jsx
@@ -1,13 +1,22 @@
+import { useState, useEffect } from "react"
 import { List, Card, Space, Button, Empty } from "antd"
 
-function ProfileAddress({ user }) {
+import { getAddresses } from "../services/user"
+
+function ProfileAddress() {
+  const [addresses, setAddresses] = useState([])
+
+  useEffect(() => {
+    getAddresses().then(setAddresses)
+  }, [])
+
   return (
     <Card title="常用地址" extra={<Button type="primary">添加</Button>}>
       <Space direction="vertical" style={{ width: "100%" }}>
-        {user.addresses.length === 0 && <Empty description="无" />}
-        {user.addresses.length > 0 && (
+        {addresses.length === 0 && <Empty description="无" />}
+        {addresses.length > 0 && (
           <List
-            dataSource={user.addresses}
+            dataSource={addresses}
             renderItem={(address) => (
               <List.Item actions={[<a onClick={() => handleDeleteAddress(address.id)}>删除</a>]}>
                 <List.Item.Meta title={`${address.receiver} ${address.tel}`} description={address.address} />
--- src/service/user.jsx
+++
@@ -1,9 +0,0 @@
-import { users } from "../data"
-
-function getAddresses() {
-  return new Promise((resolve) => {
-    resolve(users[0].addresses)
-  })
-}
-
-export { getAddresses }
