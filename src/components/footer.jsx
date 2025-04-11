dac5edbbcbdd7b7caec83d847d1bece25f6a4468
steven <1085320540@qq.com>
2025-04-11 19:29:38 +0800

    feat: add creator's link

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -5,7 +5,10 @@ const { Footer } = Layout
 function MyFooter() {
   return (
     <Footer style={{ textAlign: "center" }}>
-      BookJar ©{new Date().getFullYear()} Created by Steven
+      BookJar ©{new Date().getFullYear()} Created by{" "}
+      <a className="creator-name" href="https://github.com/stevenissleepy">
+        Steven
+      </a>
     </Footer>
   )
 }
