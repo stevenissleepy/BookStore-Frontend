ff943d9bf912098d91172f18bbb31e4964f9fa5b
steven <1085320540@qq.com>
2025-05-13 17:20:32 +0800

    feat: 增加了登录函数

--- src/services/user.jsx
+++ src/services/user.jsx
@@ -4,7 +4,7 @@ import { get, BASE_URL } from "./common"
 async function getUser() {
   const url = `${BASE_URL}/user`
   let user = null
-  try{
+  try {
     user = await get(url)
   } catch (e) {
     console.error(e)
@@ -18,4 +18,20 @@ function getAddresses() {
   })
 }
 
-export { getUser, getAddresses }
+async function login(username, password) {
+  const url = `${BASE_URL}/user/login`
+  let result
+
+  try {
+    result = await post(url, { username, password })
+  } catch (e) {
+    console.error(e)
+    result = {
+      ok: false,
+      message: "网络错误",
+    }
+  }
+  return result
+}
+
+export { getUser, getAddresses, login }
