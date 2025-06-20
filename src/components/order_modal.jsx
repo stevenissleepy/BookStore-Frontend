47debfaca3ba2a747d1ba32098e9ba48fd8a1035
steven <1085320540@qq.com>
2025-06-20 17:19:21 +0800

    fix: 管理员可以通过 order 的链接进入 user mode

--- src/components/order_modal.jsx
+++ src/components/order_modal.jsx
@@ -1,7 +1,13 @@
+import { useContext } from "react"
 import { Modal, List } from "antd"
 import { Link } from "react-router-dom"
 
+import { UserContext } from "../utils/context"
+
 function OrderDetailModel({ open, orderItems, onCancel }) {
+  const { user } = useContext(UserContext)
+  const link = user.username == "admin" ? "/admin/book" : "/book"
+
   return (
     <Modal open={open} footer={null} width={500} onCancel={onCancel} centered>
       <List
@@ -11,7 +17,7 @@ function OrderDetailModel({ open, orderItems, onCancel }) {
           <List.Item>
             <List.Item.Meta
               avatar={<img src={orderItem.book.cover} style={{ width: "50px", height: "60px", objectFit: "contain" }} />}
-              title={<Link to={`/book/${orderItem.book.id}`}>{orderItem.book.title}</Link>}
+              title={<Link to={`${link}/${orderItem.book.id}`}>{orderItem.book.title}</Link>}
               description= {`数量: ${orderItem.quantity}`}
             />
           </List.Item>
